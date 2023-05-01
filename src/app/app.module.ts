b95fd0a13822619f65dae9ff75f1e2e89847b197
Hulala <yang.ye.1@hotmail.com>
2023-05-01 18:05:06 +0200

    Rediseño 01/05/2023

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PresentacionComponent } from './presentacion/presentacion.component';
+import { LayoutHeaderComponent } from './layout/layout-header/layout-header.component';
+import { LayoutFooterComponent } from './layout/layout-footer/layout-footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    PresentacionComponent
-  ],
+    PresentacionComponent,
+    LayoutHeaderComponent,
+    LayoutFooterComponent,
+    ],
   imports: [
     BrowserModule,
     AppRoutingModule
---
+++ src/app/layout/layout-header/layout-header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layout-header',
+  templateUrl: './layout-header.component.html',
+  styleUrls: ['./layout-header.component.css']
+})
+export class LayoutHeaderComponent {
+
+}
