2d1ca35e617a676deaaaa4594a0dfdd037816119
Hulala <yang.ye.1@hotmail.com>
2023-05-19 19:32:35 +0200

    19/05/23

--- src/app/presentacion/presentacion.component.ts
+++ src/app/presentacion/presentacion.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-presentacion',
   templateUrl: './presentacion.component.html',
-  styleUrls: ['./presentacion.component.css']
+  styleUrls: ['./presentacion.component.css'],
 })
 export class PresentacionComponent {
-
+  ahora = new Date().getTime();
+  meet = new Date(2016, 4, 18).getTime();
+  diff = (this.ahora - this.meet) / 1000 / 60 / 60 / 24;
 }
